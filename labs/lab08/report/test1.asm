e61f7dc11d9cb4cac062c8306567bb19e73f5ab0
Dmitry Batov <dmitrijbatov@gmail.com>
2023-12-20 20:54:35 +0300

    lab08

---
+++ labs/lab08/report/test1.asm
@@ -0,0 +1,29 @@
+%include 'in_out.asm'
+section .data
+fun db 'Функция: f(x)=12x-7',0
+msg db 'Результат: ',0
+section .text
+global _start
+_start:
+pop ecx
+pop edx
+sub ecx,1
+mov esi,0
+next:
+cmp ecx,0h
+jz _end
+pop eax
+call atoi
+mov ebx,12
+mul ebx
+sub eax,7
+add esi,eax
+loop next
+_end:
+mov eax,fun
+call sprintLF
+mov eax,msg
+call sprint
+mov eax,esi
+call iprintLF
+call quit
